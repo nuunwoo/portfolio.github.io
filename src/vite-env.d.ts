ba7ec015f0c5b4e8b1c293276c7000323b73b305
nuunwoo <nuunwoo@gmail.com>
2025-09-17 01:19:32 +0900

    ci: deploy Vite site to GitHub Pages

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_GA_ID: string;
+  readonly VITE_SENTRY_DSN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
